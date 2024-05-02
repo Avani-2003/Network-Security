1a1f368084f1783450768d7004849c366eb59ad0
Avani Balmukund Ambekar <114228899+Avani-2003@users.noreply.github.com>
2024-05-03 01:17:20 +0530

    Update NS_Exp2.cpp

--- NS_Exp2.cpp
+++ NS_Exp2.cpp
@@ -2,95 +2,99 @@
 #include<string>
 using namespace std;
 
-string encrypt(int row, int col, string msg, int arr[])
+string encrypt(string msg,int arr[],int row, int col)
 {
-  int x=0,count=1;
-  char cipher[row][col];
+  string cipher;
+  char temp[row][col];
+  int x=0;
   for(int i=0;i<row;i++)
   {
     for(int j=0;j<col;j++)
     {
       if(x>=msg.length())
-      cipher[i][j] = '_';
-      else cipher[i][j] = msg[x];
+      temp[i][j]='_';
+      else temp[i][j] = msg[x];
       x++;
     }
   }
-  string ans;
-  while(count<=col)
+  x=1;
+  while(x<=col)
   {
     for(int i=0;i<col;i++)
     {
-      if(count==arr[i])
+      if(arr[i]==x)
       {
-        for(int j=0;j<row;j++)
-        ans += cipher[j][i];
-        break;
-      }
-    }
-    count++;
-  }
-  return ans;
-}
-
-string decrypt(int row, int col, string cipher, int arr[])
-{
-  int x=0,count=1;
-  char matrix[row][col];
-  while(count<=col)
-  {
-    for(int i=0;i<col;i++)
-    {
-      if(count==arr[i])
-      {
+        x++;
         for(int j=0;j<row;j++)
         {
-          matrix[j][i] = cipher[x];
-          x++;
+          cipher += temp[j][i];
         }
-        break;
+      } 
+    }
+  }
+  return cipher;
+}
+
+string decrypt(string cipher,int arr[],int row,int col)
+{
+  string str;
+  char temp[row][col];
+  int x=1,count=0;
+  while(x<=col)
+  {
+    for(int i=0;i<col;i++)
+    {
+      if(arr[i]==x)
+      {
+        x++;
+        for(int j=0;j<row;j++)
+        {
+          temp[j][i] = cipher[count];
+          count++;
+        }
       }
     }
-    count++;
   }
-  string ans;
   for(int i=0;i<row;i++)
   {
     for(int j=0;j<col;j++)
     {
-      if(matrix[i][j]!='_')
-      ans += matrix[i][j];
+      if(temp[i][j]!='_')
+      str += temp[i][j];
     }
   }
-  return ans;
+  return str;
 }
 
 int main()
 {
-  string msg,Key;
-  cout<<endl<<"Enter Message: ";
+  string msg,key;
+  cout<<endl<<"Message: ";
   cin>>msg;
-  cout<<endl<<"Enter Key: ";
-  cin>>Key;
-  int len_key=Key.length(),len_msg=msg.length();
-  int ind=-1,row = len_msg/len_key+1 , count=1;
+  cout<<endl<<"Key: ";
+  cin>>key;
+  int len_key,len_msg,row;
+  len_key=key.length();
+  len_msg=msg.length();
+  row = len_msg/len_key +1;
+  int count=1,ind=-1;
   int arr[len_key]={0};
   while(count<=len_key)
   {
-    int min=999;
+    int min =999;
     for(int i=0;i<len_key;i++)
     {
-      if(min>Key[i] && arr[i]==0)
+      if(min>key[i] && arr[i]==0)
       {
-        min = Key[i];
+        min=key[i];
         ind = i;
       }
     }
-    arr[ind] = count;
+    arr[ind]=count;
     count++;
   }
-  string cipher = encrypt(row,len_key,msg,arr);
+  string cipher = encrypt(msg,arr,row,len_key);
   cout<<endl<<"Cipher Text: "<<cipher;
-  cout<<endl<<"Plain Text: "<<decrypt(row,len_key,cipher,arr);
+  cout<<endl<<"Plain Text: "<<decrypt(cipher,arr,row,len_key);
   return 0;
 }
